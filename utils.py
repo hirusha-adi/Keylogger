e900cce93e44626c770f71dba06a78d5ae00f821
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-28 23:00:00 +0530

    cipher function

--- utils.py
+++ utils.py
@@ -37,7 +37,48 @@ d888  888 d888   i 888  888 d888  88b
 Y888  888 Y888   ' 888  888 Y888    , 
  "88_/888  "88_-~  888  888  "88___/  
     """
-   
+
+class Cipher:
+   def encrypt(plain_text, key=16):
+    encrypted = ""
+    for c in plain_text:
+        if c.isupper():
+            c_index = ord(c) - ord('A')
+            c_shifted = (c_index + key) % 26 + ord('A')
+            c_new = chr(c_shifted)
+            encrypted += c_new
+        elif c.islower():
+            c_index = ord(c) - ord('a') 
+            c_shifted = (c_index + key) % 26 + ord('a')
+            c_new = chr(c_shifted)
+            encrypted += c_new
+        elif c.isdigit():
+            c_new = (int(c) + key) % 10
+            encrypted += str(c_new)
+        else:
+            encrypted += c
+    return encrypted
+
+   def decrypt(ciphertext, key=16):
+    decrypted = ""
+    for c in ciphertext:
+        if c.isupper(): 
+            c_index = ord(c) - ord('A')
+            c_og_pos = (c_index - key) % 26 + ord('A')
+            c_og = chr(c_og_pos)
+            decrypted += c_og
+        elif c.islower(): 
+            c_index = ord(c) - ord('a') 
+            c_og_pos = (c_index - key) % 26 + ord('a')
+            c_og = chr(c_og_pos)
+            decrypted += c_og
+        elif c.isdigit():
+            c_og = (int(c) - key) % 10
+            decrypted += str(c_og)
+        else:
+            decrypted += c
+    return decrypted
+
 class Generate:
    class FileNames:
       base = os.getcwd()
