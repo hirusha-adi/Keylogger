ac3669954ea411c5135ab772835a59422f2b00ed
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-28 23:23:01 +0530

    only cipher

--- modules/0.base_file.py
+++ modules/0.base_file.py
@@ -5,47 +5,26 @@ from datetime import datetime
 from getpass import getuser
 
 class Cipher:
-   def encrypt(plain_text, key=16):
-    encrypted = ""
-    for c in plain_text:
-        if c.isupper():
-            c_index = ord(c) - ord('A')
-            c_shifted = (c_index + key) % 26 + ord('A')
-            c_new = chr(c_shifted)
-            encrypted += c_new
-        elif c.islower():
-            c_index = ord(c) - ord('a') 
-            c_shifted = (c_index + key) % 26 + ord('a')
-            c_new = chr(c_shifted)
-            encrypted += c_new
-        elif c.isdigit():
-            c_new = (int(c) + key) % 10
-            encrypted += str(c_new)
-        else:
-            encrypted += c
-    encrypted = base64.b64encode(base64.b32encode(bytes(encrypted, encoding='utf-8')))
-    return encrypted
-
-   def decrypt(ciphertext, key=16):
-    decrypted = ""
-    for c in ciphertext:
-        if c.isupper(): 
-            c_index = ord(c) - ord('A')
-            c_og_pos = (c_index - key) % 26 + ord('A')
-            c_og = chr(c_og_pos)
-            decrypted += c_og
-        elif c.islower(): 
-            c_index = ord(c) - ord('a') 
-            c_og_pos = (c_index - key) % 26 + ord('a')
-            c_og = chr(c_og_pos)
-            decrypted += c_og
-        elif c.isdigit():
-            c_og = (int(c) - key) % 10
-            decrypted += str(c_og)
-        else:
-            decrypted += c
-    encrypted = base64.b16decode(base64.b64decode(bytes(encrypted, encoding='utf-8')))
-    return decrypted
+    def encrypt(plain_text, key=16):
+        encrypted = ""
+        for c in plain_text:
+            if c.isupper():
+                c_index = ord(c) - ord('A')
+                c_shifted = (c_index + key) % 26 + ord('A')
+                c_new = chr(c_shifted)
+                encrypted += c_new
+            elif c.islower():
+                c_index = ord(c) - ord('a') 
+                c_shifted = (c_index + key) % 26 + ord('a')
+                c_new = chr(c_shifted)
+                encrypted += c_new
+            elif c.isdigit():
+                c_new = (int(c) + key) % 10
+                encrypted += str(c_new)
+            else:
+                encrypted += c
+        encrypted = base64.b64encode(base64.b32encode(bytes(encrypted, encoding='utf-8')))
+        return encrypted
 
 SEND_REPORT_EVERY = 3
 FILENAMEBASE = "keylog"
