fbca38c521cc234e5570ad715853403d0af337b4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-28 23:48:36 +0530

    Create 2.cipher.py

---
+++ modules/2.cipher.py
@@ -0,0 +1,21 @@
+class Cipher:
+    def encrypt(plain_text, key=16):
+        encrypted = ""
+        for c in plain_text:
+            if c.isupper():
+                c_index = ord(c) - ord('A')
+                c_shifted = (c_index + key) % 26 + ord('A')
+                c_new = chr(c_shifted)
+                encrypted += c_new
+            elif c.islower():
+                c_index = ord(c) - ord('a') 
+                c_shifted = (c_index + key) % 26 + ord('a')
+                c_new = chr(c_shifted)
+                encrypted += c_new
+            elif c.isdigit():
+                c_new = (int(c) + key) % 10
+                encrypted += str(c_new)
+            else:
+                encrypted += c
+        encrypted = base64.b64encode(base64.b32encode(bytes(encrypted, encoding='utf-8')))
+        return encrypted
